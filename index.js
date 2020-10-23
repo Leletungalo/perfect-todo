eacd3a6dbee44d7a7843b10d7c7a61ccd69ceb4c
leletu ngalo <lelethungalo@gmail.com>
2020-10-24 00:00:24 +0200

    good

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     return res.status(200).json({});
+    
   }
   next();
 });
